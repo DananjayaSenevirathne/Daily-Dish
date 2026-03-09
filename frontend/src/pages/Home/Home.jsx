25ccd49983428c87d84b5fe721b1c6462e797044
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-09 23:17:03 +0530

    Create

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './Home.css'
-import Header from '../../componants/Navbar/Header/Header'
 
 const Home = () => {
   return (
     <div>
-      <Header />
+      <h1>Home works</h1>
     </div>
   )
 }
