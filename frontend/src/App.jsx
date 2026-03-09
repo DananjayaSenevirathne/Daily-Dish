8d61f8370040f621bf73bb042f00a7219a664d45
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-09 23:16:23 +0530

    create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Navbar from './componants/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
+import ExplorMenu from './ExplorMenu/ExplorMenu'
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
       <Home />
+      <ExplorMenu />
     </div>
   )
 }
