6bef0e8c12f2462adabab03f3cbe10ed95c72869
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-10 21:07:35 +0530

    add footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Navbar from './componants/Navbar/Navbar'
 import Home from './pages/Home/Home'
+import Footer from './componants/Footer/Footer'
 
 function App() {
   return (
-    <div className='app'>
-      <Navbar />
-      <Home />
-    </div>
+    <>
+      <div className='app'>
+        <Navbar />
+        <Home />
+      </div>
+      <Footer />
+    </>
   )
 }
 
