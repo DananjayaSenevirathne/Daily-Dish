bb50dd742db66b06dd64bb2e394f0e77932c5e12
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-10 21:08:14 +0530

    edit

--- frontend/src/ExplorMenu/ExplorMenu.jsx
+++ frontend/src/ExplorMenu/ExplorMenu.jsx
@@ -10,13 +10,24 @@ const ExplorMenu = ({ category, setCategory }) => {
       </p>
 
       <div className='explore-menu-list'>
-        {menu_list.map((item, index) => {
-          return(
-          <div onClick={()=>setCategory(prev=>prev===item.menu_name? "All":item.menu_name)} key={index} className='explore-menu-list-item'>
-            <img className={category===item.menu_name?"active":""} src={item.menu_image} alt={item.menu_name} />
+        {menu_list.map((item, index) => (
+          <div
+            onClick={() =>
+              setCategory((prev) =>
+                prev === item.menu_name ? 'All' : item.menu_name
+              )
+            }
+            key={index}
+            className='explore-menu-list-item'
+          >
+            <img
+              className={category === item.menu_name ? 'active' : ''}
+              src={item.menu_image}
+              alt={item.menu_name}
+            />
             <p>{item.menu_name}</p>
           </div>
-        )})}
+        ))}
       </div>
       <hr />
     </div>
