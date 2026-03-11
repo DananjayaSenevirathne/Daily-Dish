ba8a2d453968922ba3dabad51a2e7ac9a8d590bf
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-11 15:00:19 +0530

    edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
+import React, { useState } from 'react'
 import './App.css'
 import Navbar from './componants/Navbar/Navbar'
 import Home from './pages/Home/Home'
-import Footer from './componants/Footer/Footer'
 import AppDownload from './componants/AppDownload/AppDownload'
-import { Routes, Route } from 'react-router-dom'
+import Footer from './componants/Footer/Footer'
+import LogingPopup from './componants/LogingPopup/LogingPopup'
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+      {showLogin ? <LogingPopup setShowLogin={setShowLogin} /> : null}
+
       <div className='app'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/menu' element={<Home />} />
-          <Route path='/app-download' element={<AppDownload />} />
-          <Route path='/contact' element={<div style={{ padding: '50px', fontSize: '30px' }}>Contact Page</div>} />
-        </Routes>
+        <Navbar setShowLogin={setShowLogin} />
+        <Home />
+        <AppDownload />
       </div>
+
       <Footer />
     </>
   )
