f19d72e497bb614afd592d6356cc9fc7de9c7a07
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-11 19:22:09 +0530

    fix

--- frontend/src/componants/Navbar/Navbar.jsx
+++ frontend/src/componants/Navbar/Navbar.jsx
@@ -1,58 +1,96 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import './Navbar.css'
+import { Link, useLocation } from 'react-router-dom'
 
 import logo from '../../assets/logo.png'
 import searchIcon from '../../assets/search_icon.png'
 import basketIcon from '../../assets/basket_icon.png'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState('home')
+  const { getTotalCartAmount } = useContext(StoreContext)
+  const location = useLocation()
+
+  const isHomePage = location.pathname === '/'
 
   return (
     <div className='navbar'>
-      <a href='#home'>
+      <Link to='/'>
         <img src={logo} alt='logo' className='logo' />
-      </a>
+      </Link>
 
       <ul className='navbar-menu'>
-        <a
-          href='#home'
+        <Link
+          to='/'
           onClick={() => setMenu('home')}
           className={menu === 'home' ? 'active' : ''}
         >
           Home
-        </a>
+        </Link>
 
-        <a
-          href='#explore-menu'
-          onClick={() => setMenu('menu')}
-          className={menu === 'menu' ? 'active' : ''}
-        >
-          Menu
-        </a>
+        {isHomePage ? (
+          <>
+            <a
+              href='#explore-menu'
+              onClick={() => setMenu('menu')}
+              className={menu === 'menu' ? 'active' : ''}
+            >
+              Menu
+            </a>
 
-        <a
-          href='#app-download'
-          onClick={() => setMenu('mobile-app')}
-          className={menu === 'mobile-app' ? 'active' : ''}
-        >
-          Mobile-app
-        </a>
+            <a
+              href='#app-download'
+              onClick={() => setMenu('mobile-app')}
+              className={menu === 'mobile-app' ? 'active' : ''}
+            >
+              Mobile-app
+            </a>
 
-        <a
-          href='#footer'
-          onClick={() => setMenu('contact-us')}
-          className={menu === 'contact-us' ? 'active' : ''}
-        >
-          Contact us
-        </a>
+            <a
+              href='#footer'
+              onClick={() => setMenu('contact-us')}
+              className={menu === 'contact-us' ? 'active' : ''}
+            >
+              Contact us
+            </a>
+          </>
+        ) : (
+          <>
+            <Link
+              to='/'
+              onClick={() => setMenu('menu')}
+              className={menu === 'menu' ? 'active' : ''}
+            >
+              Menu
+            </Link>
+
+            <Link
+              to='/'
+              onClick={() => setMenu('mobile-app')}
+              className={menu === 'mobile-app' ? 'active' : ''}
+            >
+              Mobile-app
+            </Link>
+
+            <Link
+              to='/'
+              onClick={() => setMenu('contact-us')}
+              className={menu === 'contact-us' ? 'active' : ''}
+            >
+              Contact us
+            </Link>
+          </>
+        )}
       </ul>
 
       <div className='navbar-right'>
         <img src={searchIcon} alt='search' />
         <div className='navbar-search-icon'>
-          <img src={basketIcon} alt='basket' />
-          <div className='dot'></div>
+          <Link to='/cart'>
+            <img src={basketIcon} alt='basket' />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
 
         <button onClick={() => setShowLogin(true)}>sign in</button>
