26b98b93653f1e2bab3962f711094489bdeef14a
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-11 12:15:24 +0530

    create

---
+++ frontend/src/componants/AppDownload/AppDownload.jsx
@@ -0,0 +1,20 @@
+import './AppDownload.css'
+import play_store from '../../assets/play_store.png'
+import app_store from '../../assets/app_store.png'
+
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+      <p>
+        For Better Experience Download <br /> Tomato App
+      </p>
+
+      <div className='app-download-platforms'>
+        <img src={play_store} alt='Google Play' />
+        <img src={app_store} alt='App Store' />
+      </div>
+    </div>
+  )
+}
+
+export default AppDownload
