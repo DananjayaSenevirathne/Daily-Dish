ebe6d2d62b60a0909f72aa0377c5e61ba58a9488
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-11 12:14:58 +0530

    add th app download

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 import Navbar from './componants/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Footer from './componants/Footer/Footer'
+import AppDownload from './componants/AppDownload/AppDownload'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <div className='app'>
         <Navbar />
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/menu' element={<Home />} />
+          <Route path='/app-download' element={<AppDownload />} />
+          <Route path='/contact' element={<div style={{ padding: '50px', fontSize: '30px' }}>Contact Page</div>} />
+        </Routes>
       </div>
       <Footer />
     </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import StoreContextProvider from './context/StoreContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
+    <BrowserRouter>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
