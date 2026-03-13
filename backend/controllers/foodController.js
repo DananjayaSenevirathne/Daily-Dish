0afdf8ad3ef285687ca43e4da2687eac8c19b9d7
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-13 22:27:44 +0530

    fix

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,12 @@
+import foodModels from "../models/foodModels";
+import fs from "fs";
+
+
+//add food item
+
+const addFoodItem = async (req, res) => {
+let image_filename = null;
+}
+
+
+export { addFood };
