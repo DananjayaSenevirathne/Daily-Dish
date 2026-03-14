68ef034240e408561c25e27c721cc798db27f579
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-03-14 17:09:37 +0530

    fix

--- admin/src/components/sidebar/sidebar.jsx
+++ admin/src/components/sidebar/sidebar.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import add_icon from '../../assets/add_icon.png'
 import order_icon from '../../assets/order_icon.png'
+import './Sidebar.css'
+import { NavLink } from 'react-router-dom'
 
-
-import './sidebar.css'
-const sidebar = () => {
+const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <div className="sidebar-options">
-            <div className="sidebar-option">
-                <img src={add_icon} alt="" className="" />
-                <p className=""> Add items</p>
-            </div>
+      <div className="sidebar-options">
+        <NavLink to="/add" className="sidebar-option">
+          <img src={add_icon} alt="Add items" />
+          <p>Add Items</p>
+        </NavLink>
 
-            <div className="sidebar-option">
-                <img src={order_icon} alt="" className="" />
-                <p className=""> List items</p>
-            </div>
+        <NavLink to="/list" className="sidebar-option">
+          <img src={order_icon} alt="List items" />
+          <p>List Items</p>
+        </NavLink>
 
-            <div className="sidebar-option">
-                <img src={order_icon} alt="" className="" />
-                <p className=""> Order items</p>
-            </div>
+        <NavLink to="/orders" className="sidebar-option">
+          <img src={order_icon} alt="Orders" />
+          <p>Orders</p>
+        </NavLink>
 
-        </div>
-      
+        
+      </div>
     </div>
   )
 }
 
-export default sidebar
+export default Sidebar
