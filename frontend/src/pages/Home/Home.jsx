2c1be22cef822acba8f0bc97b92e1d2bd87886d9
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-05-19 12:05:30 +0530

    fix

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,29 @@
+import React, { useState } from 'react'
 import './Home.css'
+
 import Header from '../../componants/Navbar/Header/Header'
 import ExplorMenu from '../../ExplorMenu/ExplorMenu'
 import FoodDisplay from '../../componants/FoodDisplay/FoodDisplay'
 import AppDownload from '../../componants/AppDownload/AppDownload'
-import { useState } from 'react'
 
 const Home = () => {
+
   const [category, setCategory] = useState('All')
 
   return (
     <div>
+
       <Header />
-      <ExplorMenu category={category} setCategory={setCategory} />
+
+      <ExplorMenu
+        category={category}
+        setCategory={setCategory}
+      />
+
       <FoodDisplay category={category} />
+
       <AppDownload />
+
     </div>
   )
 }
