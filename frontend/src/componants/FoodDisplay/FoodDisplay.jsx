67f2d12883dab6f2ebb84d8dd4e2b9c0380ee024
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-05-20 15:47:45 +0530

    fix

--- frontend/src/componants/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/componants/FoodDisplay/FoodDisplay.jsx
@@ -1,32 +1,56 @@
-import React from 'react'
-import { useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import Fooditem from '../Fooditem/Fooditem'
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import Fooditem from "../Fooditem/Fooditem";
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext)
+
+  const { food_list } = useContext(StoreContext);
+
+  console.log(food_list);
 
   return (
-    <div className='food-display' id='food-display'>
+
+    <div className="food-display" id="food-display">
+
       <h2>Top dishes near you</h2>
 
-      <div className='food-display-list'>
-        {food_list
-          .filter((item) => category === 'All' || category === item.category)
-          .map((item) => (
-            <Fooditem
-                 key={item._id}
-                 id={item._id}
-                 name={item.name}
-                 description={item.description}
-                 price={item.price}
-                 image={item.image}
-                />
-          ))}
-      </div>
-    </div>
-  )
-}
+      <div className="food-display-list">
 
-export default FoodDisplay
+        {food_list && food_list.length > 0 ? (
+
+          food_list.map((item) => {
+
+            if (
+              category === "All" ||
+              category === item.category
+            ) {
+
+              return (
+                <Fooditem
+                  key={item._id}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                />
+              );
+            }
+
+            return null;
+          })
+
+        ) : (
+
+          <p>Loading foods...</p>
+
+        )}
+
+      </div>
+
+    </div>
+  );
+};
+
+export default FoodDisplay;
