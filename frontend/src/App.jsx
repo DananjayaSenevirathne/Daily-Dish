3c18927e11f0fd50fce27eaa7ae7716b9fc492e2
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-05-23 13:25:21 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import LogingPopup from './componants/LogingPopup/LogingPopup'
 import { Routes, Route } from 'react-router-dom'
+import Verify from './pages/Verify/Verify'
+import MyOrders from './pages/MyOrders/MyOrders'
 
 function App() {
   const [showLogin, setShowLogin] = useState(false)
@@ -21,6 +23,8 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/verify' element={<Verify/>} />
+          <Route path='/myorders' element={<MyOrders/>} />
         </Routes>
       </div>
 
